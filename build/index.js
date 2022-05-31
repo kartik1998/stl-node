606a68caa6aa63e2cbb5be75fafad48489aadfcc
kartik1998 <kartikrawat61@gmail.com>
2022-05-31 22:09:42 +0530

    remove build from git

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Stack = void 0;
-const stack_1 = __importDefault(require("./stack/stack"));
-exports.Stack = stack_1.default;
