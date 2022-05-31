4c744f450d16784717525960a8962032da31a99b
kartik1998 <kartikrawat61@gmail.com>
2022-05-31 22:08:26 +0530

    add search method in stack

--- src/stack/stack.ts
+++ src/stack/stack.ts
@@ -9,15 +9,18 @@ export default class Stack<T> {
     this._arr.push(value);
   }
 
+  // removes top element from stack or returns undefined
   public pop(): T | undefined {
     return this._arr.pop();
   }
 
+  // Returns the element on the top of the stack, but does not remove it.
   public peek(): T | undefined {
     if (this.isEmpty()) return undefined;
     return this._arr[this._arr.unshift()];
   }
 
+  // checks if stack is empty
   public isEmpty(): boolean {
     return this._arr.length === 0;
   }
@@ -34,6 +37,15 @@ export default class Stack<T> {
     this._arr = [];
   }
 
+  // searches and checks if an object is in stack
+  public search(obj: T): boolean {
+    for (const val of this._arr) {
+      if (val === obj) return true;
+    }
+    return false;
+  }
+
+  // creates a stack from an array
   public static fromArray<T>(array: Array<T>) {
     return new Stack<T>(array);
   }
